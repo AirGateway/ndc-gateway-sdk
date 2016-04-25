57b17de810adc69f1a29ab1e3c5be61d07d63dbb
Matias Insaurralde <matias@insaurral.de>
2016-04-25 13:46:35 -0400

    Basic client structure

---
+++ ndc_client/ndc_client.go
@@ -0,0 +1,28 @@
+package ndc
+
+
+const (
+	AcceptedContentType string = "application/xml"
+)
+
+var NDCSupportedMethods = map[string] struct{} {
+	"AirShoppingRQ": {},
+	"FlightPriceRQ": {},
+	"SeatAvailabilityRQ": {},
+	"ServiceListRQ": {},
+	"ServicePriceRQ": {},
+	"OrderCreateRQ": {},
+	"OrderRetrieveRQ": {},
+	"OrderListRQ": {},
+	"OrderCancelRQ": {},
+	"ItinReshopRQ": {},
+}
+
+type Client struct {
+  Endpoint string
+  Config string
+}
+
+func (r *Client) SendRequest( Method string, Data string ) {
+  return
+}
